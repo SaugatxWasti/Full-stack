a990b701e795fa0a8b57ccb73ae9bf4a879d6f96
SaugatxWasti <wastisaugat217@gmail.com>
2026-01-14 23:03:48 +0545

    javascript

---
+++ module07/script.js
@@ -0,0 +1,29 @@
+// console.log("page sucessfully loaded")
+
+
+// const name = "saugat wasti";   //string
+// let profession = "programmer";
+
+// const str = "saugat";  //string
+// const num = 3;  //number
+// const flt = 3.33;  //float number
+// const bool = true;  // or false
+// const list = ["item 1", 3, 2, "saugat"];   // array = list
+// const thing = null;   // has nothing in it
+// const notDefined = undefined; // currently holds no value
+// const getAge = function() {return 31;} // This is a function
+// const obj={
+//     'name' : 'saugat',
+//     'profession': 'programmer'
+// }
+
+const age = 18;
+if (age >=18){
+    console.log("Is allowed to vote");
+}
+if(age>=21){
+    console.log("is allowed to drink alcohol")
+}
+else{
+    console.log("not allowed to vote and drink")
+}
